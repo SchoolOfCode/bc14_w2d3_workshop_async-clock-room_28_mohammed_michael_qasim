d8a00f4070415d6a5e2b1b39702fcbe4f3ed5301
Mfar121 <127731220+Mfar121@users.noreply.github.com>
2023-03-22 13:37:52 +0000

    clock ticks forever

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 const hand = document.querySelector(".hand");
 
-hand.style.transform = `rotate(90deg)`; //🔍 HINT: THis is an example of how to rotate the hand!
+// hand.style.transform = `rotate(90deg)`; //🔍 HINT: THis is an example of how to rotate the hand!
+
+// create function that moves the hand 6 degrees 
+//create setInterval and feed above function in to it
+// make it rotate the hand 6degrees every 1000 ms
+
+let currentAngle = 0
+
+function rotate(){
+
+let newAngle = currentAngle +6 
+
+hand.style.transform = `rotate(${newAngle}deg)`
+
+currentAngle=newAngle
+
+}
+
+setInterval(rotate, 1000);
+
